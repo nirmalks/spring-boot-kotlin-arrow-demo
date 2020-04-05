f7d96c36f2da1f88a06d1415ef2e209b973071a4
Nirmal Kumar <kagosekar@gmail.com>
2020-04-05 10:54:03 +0530

    Add arrow and use IO
    * Add arrow dependency
    * Use IO for services with DB and replace controller methods
    * Use suspend for db related functions

--- src/main/kotlin/com/example/demo/controller/CricketerController.kt
+++ src/main/kotlin/com/example/demo/controller/CricketerController.kt
@@ -1,5 +1,8 @@
 package com.example.demo.controller
 
+import arrow.core.Either
+import arrow.core.None
+import arrow.core.Some
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.http.ResponseEntity
@@ -13,45 +16,70 @@ import org.springframework.web.bind.annotation.RequestMapping
 import com.example.demo.service.CricketerService
 import com.example.demo.repository.CricketerRepository
 import com.example.demo.model.Cricketer
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api")
 class CricketerController(private val cricketerService: CricketerService , private val cricketerRepository: CricketerRepository){
-	
 	@GetMapping("/cricketers/{id}")
-	fun getCricketer(@PathVariable("id") id: Long):ResponseEntity<Cricketer>  {
-		val cricketer = cricketerService.findById(id)
-		return ResponseEntity<Cricketer>(cricketer as Cricketer, HttpStatus.OK);
+	suspend fun getCricketer(@PathVariable("id") id: Long):ResponseEntity<Cricketer>  {
+		return when(val cricketer = cricketerService.findById(id).attempt().unsafeRunSync()) {
+			is Either.Left ->  throw ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR, "Unable to fetch cricketer")
+			is Either.Right-> {
+				when(cricketer.b) {
+					is Some -> ResponseEntity(cricketer.b.orNull(), HttpStatus.OK)
+					is None -> throw ResponseStatusException(
+							HttpStatus.NOT_FOUND, "Cricketer with id $id not found")
+				}
+			}
+		}
 	}
 	
-	@GetMapping("/cricketers/")
-	fun getAllCricketers() :ResponseEntity<List<Cricketer>>  {
-		var cricketersList: ArrayList<Cricketer> =  cricketerService.getAllPlayers() as (ArrayList<Cricketer>)
-		return ResponseEntity<List<Cricketer>>(cricketersList, HttpStatus.OK)
+	@GetMapping("/cricketers")
+	suspend fun getAllCricketers() :ResponseEntity<List<Cricketer>>  {
+		return when(val cricketers = cricketerService.getAllPlayers().attempt().unsafeRunSync()) {
+			is Either.Left -> throw ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR, "Unable to fetch cricketers")
+			is Either.Right-> {
+				return ResponseEntity(cricketers.b, HttpStatus.OK)
+			}
+		}
 	}
 	
-	@PostMapping("/cricketer/")
-	fun addCricketer(@RequestBody cricketer:Cricketer):ResponseEntity<Cricketer>  {
-		val cCricketer : Cricketer = Cricketer(name = cricketer.name
-				, country = cricketer.country
-				, highestScore = cricketer.highestScore)
-		cricketerRepository.save(cCricketer)
-		return ResponseEntity<Cricketer>(cricketer , HttpStatus.OK)
+	@PostMapping("/cricketer")
+	suspend fun addCricketer(@RequestBody cricketer:Cricketer):ResponseEntity<Cricketer>  {
+		when(val cricketer = cricketerService.save(cricketer).attempt().unsafeRunSync()) {
+			is Either.Left -> throw ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR, "Unable to save cricketer")
+			is Either.Right-> {
+				return ResponseEntity(cricketer.b , HttpStatus.OK)
+			}
+		}
 	}
 	
 	@PutMapping("/cricketer/{id}")
-	fun updateCricketer(@PathVariable("id") id: Long, @RequestBody cricketer: Cricketer ):ResponseEntity<Cricketer> {
-		val cCricketer  = Cricketer(name = cricketer.name
+	suspend fun updateCricketer(@PathVariable("id") id: Long, @RequestBody cricketer: Cricketer ):ResponseEntity<Cricketer> {
+		val updatedCricketer  = Cricketer(name = cricketer.name
 				, country = cricketer.country
 				, highestScore = cricketer.highestScore)
-		cricketerRepository.save(cCricketer)
-		return ResponseEntity<Cricketer>(cricketer, HttpStatus.OK)
+		when(val cricketer = cricketerService.save(updatedCricketer).attempt().unsafeRunSync()) {
+			is Either.Left -> throw ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR, "Unable to update cricketer $id")
+			is Either.Right-> {
+				return ResponseEntity(cricketer.b , HttpStatus.OK)
+			}
+		}
 	}
 	
 	@DeleteMapping("/cricketer/{id}")
-	fun deleteCricketer(@PathVariable("id") id:Long ):ResponseEntity<String> {
-		val cCricketer :Cricketer  = cricketerService.findById(id) as Cricketer
-		cricketerRepository.delete(cCricketer)
-		return ResponseEntity<String>("cricketer removed", HttpStatus.OK)
+	suspend fun deleteCricketer(@PathVariable("id") id:Long ):ResponseEntity<String> {
+		when(cricketerService.deleteById(id).attempt().unsafeRunSync()) {
+			is Either.Left -> throw ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR, "Unable to delete cricketer $id")
+			is Either.Right-> {
+				return ResponseEntity(HttpStatus.OK)
+			}
+		}
 	}
 }
--- src/main/kotlin/com/example/demo/service/CricketerService.kt
+++ src/main/kotlin/com/example/demo/service/CricketerService.kt
@@ -1,20 +1,37 @@
 package com.example.demo.service
+import arrow.core.Option
+import arrow.core.toOption
+import arrow.fx.IO
+import arrow.fx.extensions.fx
 import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Autowired
 import com.example.demo.repository.CricketerRepository
 import com.example.demo.model.Cricketer
+import java.util.*
 
 @Service
-class CricketerService(@Autowired var cricketerRepository:CricketerRepository) {
-	fun save(cricketer: Cricketer ): Unit {
-		cricketerRepository.save(cricketer)
+class CricketerService(@Autowired val cricketerRepository:CricketerRepository) {
+	suspend fun save(cricketer: Cricketer ): IO<Cricketer> {
+		return IO {
+			cricketerRepository.save(cricketer)
+		}
 	}
 
-	fun findById(id:Long):Any {
-		return cricketerRepository.findById(id)
+	suspend fun findById(id:Long): IO<Option<Cricketer>> {
+		return IO {
+			cricketerRepository.findById(id).get().toOption()
+		}
 	}
-		
-	fun getAllPlayers():List<Cricketer> {
-		return cricketerRepository.findAll()
+
+	suspend fun getAllPlayers(): IO<List<Cricketer>> {
+		return IO {
+			cricketerRepository.findAll()
+		}
+	}
+
+	suspend fun deleteById(id: Long): IO<Unit> {
+		return IO {
+			cricketerRepository.deleteById(id)
+		}
 	}
 }
